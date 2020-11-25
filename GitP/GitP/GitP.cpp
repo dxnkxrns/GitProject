085d4fe9d27cf053066346aad1edf82271bf1dd5
kreker_231 <kohanovichartem@gmail.com>
2020-11-25 22:33:44 +0300

    add switch

--- GitP/GitP/GitP.cpp
+++ GitP/GitP/GitP.cpp
@@ -2,12 +2,41 @@
 #include <iostream>
 #include "function.h"
 using namespace std;
+
 int main()
 {
-	cout << Sum(10, 3) << endl;
-	cout << Minus(10, 3) << endl;
-	cout << Mul(10, 3) << endl;
-	cout << Div(10, 3) << endl;
+	setlocale(LC_ALL, "Russian");
+	int c;
+	double a, b;
+	cout << "Введите a" << endl;
+	cin >> a;
+	cout << "Введите b" << endl;
+	cin >> b;
+	cout << "Введите номер функции" << endl;
+	cin >> c;
+	switch (c)
+	{
+	case 1:
+		cout << "Сумма a и b = ";
+		cout << Sum(a, b) << endl;
+		break;
+	case 2:
+		cout << "Разность a и b = ";
+		cout << Minus(a, b) << endl;
+		break;
+	case 3:
+		cout << "Произведение a и b = ";
+		cout << Mul(a, b) << endl;
+		break;
+	case 4:
+		cout << "Частное a/b = ";
+		cout << Div(a, b) << endl;
+		break;
+	
+	default:
+		cout << "Такой операции нет";
+		break;
+	}
 	system("pause");
 	return 0;
 }
