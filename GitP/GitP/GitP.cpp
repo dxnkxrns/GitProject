b98ebda1c1e8471e13f2b425cf40ca581d5b57e3
Daniil <dynkachannel@gmail.com>
2020-11-25 22:46:03 +0300

    Add COUT and remake text

--- GitP/GitP/GitP.cpp
+++ GitP/GitP/GitP.cpp
@@ -8,28 +8,32 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	int c;
 	double a, b;
-	cout << "Введите a" << endl;
+	cout << "Введите a: " << endl;
 	cin >> a;
-	cout << "Введите b" << endl;
+	cout << "Введите b: " << endl;
 	cin >> b;
-	cout << "Введите номер функции" << endl;
+	cout << "Введите номер функции: " << endl;
+	cout << "1. Функция суммы" << endl;
+	cout << "2. Функция разности" << endl;
+	cout << "3. Функция произведения" << endl;
+	cout << "4. Функция частное" << endl;
 	cin >> c;
 	switch (c)
 	{
 	case 1:
-		cout << "Сумма a и b = ";
+		cout << "A+B = ";
 		cout << Sum(a, b) << endl;
 		break;
 	case 2:
-		cout << "Разность a и b = ";
+		cout << "A-B = ";
 		cout << Minus(a, b) << endl;
 		break;
 	case 3:
-		cout << "Произведение a и b = ";
+		cout << "A*B = ";
 		cout << Mul(a, b) << endl;
 		break;
 	case 4:
-		cout << "Частное a/b = ";
+		cout << "A/B = ";
 		cout << Div(a, b) << endl;
 		break;
 	
